8d5da32d7057a0d291d0958c91dc79027789a216
Eugene Sia <eugene@eugenesia.co.uk>
2018-10-08 17:19:00 +0100

    Create courseReducer

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,13 @@
+// Start with state = [] (no courses)
+export default function courseReducer(state = [], action) {
+  switch (action.type) {
+    case 'CREATE_COURSE':
+      return [...state,
+        // Create deep copy of action.course for immutability.
+        Object.assign({}, action.course)
+      ];
+
+    default:
+      return state;
+  }
+}

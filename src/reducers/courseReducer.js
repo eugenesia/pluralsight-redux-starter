f256e876ac40b83fc6537f50eaae37ac04935589
Eugene Sia <eugene@eugenesia.co.uk>
2018-10-09 16:30:11 +0100

    Define store initial state

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,7 +1,8 @@
 import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
 // Start with state = [] (no courses)
-export default function courseReducer(state = [], action) {
+export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;

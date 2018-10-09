5a37b7b54001d296462cbcd799d98a9d3f5fcf70
Eugene Sia <eugene@eugenesia.co.uk>
2018-10-09 13:46:56 +0100

    Create thunk loadCourses()

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,4 +1,17 @@
 import * as types from './actionTypes';
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course};
+import courseApi from '../api/mockCourseApi';
+
+export function loadCoursesSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+export function loadCourses() {
+  // A thunk always returns a function that accepts dispatch().
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
 }

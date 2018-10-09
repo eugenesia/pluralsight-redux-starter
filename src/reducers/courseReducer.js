5d81b0a16d2c368801db3e01a8961bee89724571
Eugene Sia <eugene@eugenesia.co.uk>
2018-10-09 17:35:29 +0100

    Add create/update reducers

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -6,6 +6,22 @@ export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
+    
+    case types.CREATE_COURSE_SUCCESS:
+      // Add newly created course to state.
+      return [
+        ...state,
+        // Object.assign() to make sure we copy instead of using existing ref.
+        Object.assign({}, action.course)
+      ];
+    
+    case types.UPDATE_COURSE_SUCCESS:
+      return [
+        // Remove the updated course in the old state.
+        ...state.filter(course => course.id !== action.course.id),
+        // Add updated course with new data into state.
+        Object.assign({}, action.course)
+      ];
 
     default:
       return state;

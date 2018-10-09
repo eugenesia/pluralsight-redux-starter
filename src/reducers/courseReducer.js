96575628fec5f43b450c63d4ff7e6e7b63431348
Eugene Sia <eugene@eugenesia.co.uk>
2018-10-09 14:05:55 +0100

    Delete CREATE_COURSE action

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -3,11 +3,6 @@ import * as types from '../actions/actionTypes';
 // Start with state = [] (no courses)
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case types.CREATE_COURSE:
-      return [...state,
-        // Create deep copy of action.course for immutability.
-        Object.assign({}, action.course)
-      ];
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
 

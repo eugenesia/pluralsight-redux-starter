ed1ad3d40d63e40665234b825adc17d741c513be
Eugene Sia <eugene@eugenesia.co.uk>
2018-10-10 00:06:21 +0100

    Create ajaxStatusReducer, add to rootReducer

---
+++ src/reducers/ajaxStatusReducer.js
@@ -0,0 +1,18 @@
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
+
+function actionTypeEndsInSuccess(type) {
+  return type.substring(type.length - 8) === '_SUCCESS';
+}
+
+export default function ajaxStatusReducer(state = initialState.numAjaxCallsInProgress, action) { 
+  if (action.type === types.BEGIN_AJAX_CALL) {
+    // Increment number of Ajax calls being made.
+    return state + 1;
+  } else if (actionTypeEndsInSuccess(action.type)) {
+    // Handle any action type that ends in '_SUCCESS'.
+    // If action ends in success, decrement no of calls as call ended.
+    return state - 1;
+  }
+  return state;
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 // Object passed in determines shape of state https://redux.js.org/api/combinereducers
 const rootReducer = combineReducers({
   // state.courses is managed by the courseReducer
   courses,
-  authors
+  authors,
+  ajaxCallsInProgress
 });
 
 export default rootReducer;

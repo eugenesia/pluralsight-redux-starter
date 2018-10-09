a5c925f85e239ad2d2a17e5d22547c64d12034e1
Eugene Sia <eugene@eugenesia.co.uk>
2018-10-09 14:48:26 +0100

    Create ManageCoursePage

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
+import ManageCoursePage from './components/course/ManageCoursePage';
 
 export default (
   <Route path="/" component={App}>
     {/* Set child component based on route. */}
     <IndexRoute component={HomePage} />
     <Route path="courses" component={CoursesPage} />
+    <Route path="course/:id" component={ManageCoursePage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );

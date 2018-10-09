997089aa841ef8c406974b22d0fc78d5ed062901
Eugene Sia <eugene@eugenesia.co.uk>
2018-10-09 16:31:17 +0100

    Add authorReducer to  rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
 
 // Object passed in determines shape of state https://redux.js.org/api/combinereducers
 const rootReducer = combineReducers({
   // state.courses is managed by the courseReducer
-  courses
+  courses,
+  authors
 });
 
 export default rootReducer;

645bec832a0dc0140e8b94d1e9a110f6c19a162e
Eugene Sia <eugene@eugenesia.co.uk>
2018-10-09 17:28:40 +0100

    Create actions for create/update course

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,11 +5,32 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+export function createCourseSuccess(course) {
+  return {type: types.CREATE_COURSE_SUCCESS, course};
+}
+
+export function updateCourseSuccess(course) {
+  return {type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
 export function loadCourses() {
   // A thunk always returns a function that accepts dispatch().
   return function(dispatch) {
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      // If course has ID, it's existing course and we're updating.
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        // Else we're creating a new course.
+        dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
       throw(error);
     });
